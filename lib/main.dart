5162d41ffa2a361649a796ab83c5392d30723e0e
Dew3 <77088465+Dew3@users.noreply.github.com>
2022-02-07 17:26:24 -0500

    pull

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Changes',
+      title: 'Pull Test',
       theme: ThemeData(
         // This is the theme of your application.
         //
